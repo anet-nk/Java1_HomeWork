4be3793e125006c5de234c53b8f72b916722a209
Anet <tsurupa.nk@gmail.com>
2021-10-03 21:28:50 +0300

    Home Work 3

---
+++ JavaCore_HW_3/Apple.java
@@ -0,0 +1,13 @@
+package Lesson3;
+
+public class Apple extends Fruit{
+
+    public Apple() {
+        super(1.0f);
+    }
+
+    @Override
+    public String toString() {
+        return "Apple" ;
+    }
+}
---
+++ JavaCore_HW_3/Orange.java
@@ -0,0 +1,16 @@
+package Lesson3;
+
+import java.util.ArrayList;
+
+public class Orange extends Fruit{
+
+    public Orange() {
+        super(1.5f);
+    }
+
+    @Override
+    public String toString() {
+        return "Orange" ;
+    }
+
+}
